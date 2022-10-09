95e93ff51bf163b8eacd49b16721ef21a3a6afe5
Alberto <albertiae.digital@gmail.com>
2022-10-09 18:34:17 -0400

    Created scrollReveal.js for revealing the elements as the user scrolls down

---
+++ js/scrollReveal.js
@@ -0,0 +1,11 @@
+//scroll reveal
+
+ScrollReveal().reveal('.banner', { delay: 500 });
+
+ScrollReveal().reveal('.under-banner', { delay: 500 });
+
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+
+ScrollReveal().reveal('.social', { delay: 500 });
+
+ScrollReveal().reveal('.banner-down', { delay: 500 });
