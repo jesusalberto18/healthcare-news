8d682f4fad8dda94b055bca7b3a784b9a787c008
Alberto <albertiae.digital@gmail.com>
2022-10-09 15:07:04 -0400

    Added a query selector

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+// confirmation for developers
 console.log("This message in a confirmation for all the developers that this script is correctly running.")
+
+// main.js
+
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+  document.querySelector('.menu').classlist.toggle('show');
+})
