ba506f5c3f005fb81eeadb2bfa94988a0f973c65
Alberto <albertiae.digital@gmail.com>
2022-10-09 18:39:59 -0400

    Modified the delay in the reveal

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,11 +1,11 @@
 //scroll reveal
 
-ScrollReveal().reveal('.banner', { delay: 500 });
+ScrollReveal().reveal('.banner', { delay: 1000 });
 
-ScrollReveal().reveal('.under-banner', { delay: 500 });
+ScrollReveal().reveal('.under-banner', { delay: 1000 });
 
-ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.news-cards', { delay: 1000 });
 
-ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 1000 });
 
-ScrollReveal().reveal('.banner-down', { delay: 500 });
+ScrollReveal().reveal('.banner-down', { delay: 1000 });
