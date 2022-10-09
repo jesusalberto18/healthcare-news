9a67fa576f645ea718d8d6890975448049d7d9b6
Alberto <albertiae.digital@gmail.com>
2022-10-09 17:04:01 -0400

    Modified the event listener in the toggle menu script

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // confirmation for developers
-console.log("This message in a confirmation for all the developers that this script is correctly running.")
+console.log("This message is a confirmation for all the developers that this script is correctly running.")
 
 // main.js
 
-document.querySelector('.menu-btn').addEventListener('click', () =>{
-  document.querySelector('.menu').classlist.toggle('show');
-})
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.querySelector('.menu').classList.toggle('show');
+});
